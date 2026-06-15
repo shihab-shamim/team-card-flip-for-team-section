f5700b86e2c0cefb09bc99a5a32450b785a5ba51
shihab-shamim <shihabshamim767@gmail.com.com>
2026-06-15 16:56:11 +0600

    styles code align

--- src/Components/Backend/Settings/Style/Style.js
+++ src/Components/Backend/Settings/Style/Style.js
@@ -1,117 +1,117 @@
-import { useState } from "react";
-import { __ } from "@wordpress/i18n";
-import {
-  PanelBody,
-  PanelRow,
-  RangeControl,
-  SelectControl,
-  __experimentalUnitControl as UnitControl,BorderControl
-} from "@wordpress/components";
-import { Background, BoxControl, ColorControl, ColorsControl, Device, Label, Typography } from "../../../../../../bpl-tools/Components";
-import { updateData } from "../../../../utils/functions";
+  import { useState } from "react";
+  import { __ } from "@wordpress/i18n";
+  import {
+    PanelBody,
+    PanelRow,
+    RangeControl,
+    SelectControl,
+    __experimentalUnitControl as UnitControl,BorderControl
+  } from "@wordpress/components";
+  import { Background, BoxControl, ColorControl, ColorsControl, Device, Label, Typography } from "../../../../../../bpl-tools/Components";
+  import { updateData } from "../../../../utils/functions";
 
-const Style = ({ attributes, setAttributes,device }) => {
-  const { styles={} } = attributes;
+  const Style = ({ attributes, setAttributes,device }) => {
+    const { styles={} } = attributes;
 
 
 
-  return (
-    <>
-      <PanelBody
-          
-            className="bPlPanelBody"
-            title={__("Container", "team-section")}
-            initialOpen={false}
-          >
-            <Background label={__("Background", "team-section")} value={styles?.bg} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"bg")})}  />
-
-              <PanelRow><Label>Padding</Label><Device/></PanelRow>
+    return (
+      <>
+             <PanelBody
             
-            <BoxControl  values={styles?.padding[device]} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"padding",device)})}/>
+              className="bPlPanelBody"
+              title={__("Container", "team-section")}
+              initialOpen={false}
+            >
+              <Background label={__("Background", "team-section")} value={styles?.bg} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"bg")})}  />
 
-                <PanelRow><Label>Margin</Label><Device/></PanelRow>
+                <PanelRow><Label>Padding</Label><Device/></PanelRow>
+              
+              <BoxControl  values={styles?.padding[device]} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"padding",device)})}/>
+
+                  <PanelRow><Label>Margin</Label><Device/></PanelRow>
+              
+              <BoxControl  values={styles?.margin[device]} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"margin",device)})}/>
+              
+              <BoxControl  className='mt15' label="Radius"  values={styles?.radius} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"radius")})}/>
+
             
-            <BoxControl  values={styles?.margin[device]} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"margin",device)})}/>
+
+            </PanelBody>
+
+            <PanelBody className="bPlPanelBody"
+              title={__("Team Profile", "team-section")}
+              initialOpen={false}>
+
+                <Background label={"Background"} value={styles?.teamProfile?.bg} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"teamProfile","bg")})} />
+
+                  <BoxControl label="Padding" className="mt15" values={styles?.teamProfile?.padding} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"teamProfile","padding")})}/>
+
+                    <ColorControl label="Text Color" className="mt15" value={styles?.teamProfile?.color} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"teamProfile","color")})}/>
+
+                    <SelectControl label="Text Align" className="mt15" value={styles?.teamProfile?.textAlign} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"teamProfile","textAlign")})}
+                    options={[
+                      { value: "center", label: "Center" },
+                      { value: "left", label: "Left" },
+                      { value: "right", label: "Right" }
+                    ]}/>
+
+                      <BoxControl label="Name Margin" className="mt15" values={styles?.teamProfile?.name?.margin} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"teamProfile","name","margin")})}/>
+
+                          <Typography label="Name Typography" className="mt15" value={styles?.teamProfile?.name?.typo} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"teamProfile","name","typo")})}/>
+
+                            <BoxControl label="Designation Margin" className="mt15" values={styles?.teamProfile?.designation?.margin} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"teamProfile","designation","margin")})}/>
+
+                          <Typography label="Designation Typography" className="mt15" value={styles?.teamProfile?.designation?.typo} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"teamProfile","designation","typo")})}/>
+
+                
+
+            </PanelBody>
+
+            <PanelBody  className="bPlPanelBody"
+              title={__("Image", "team-section")}
+              initialOpen={false}>
+                
+                <UnitControl className='mt15' label="Width" value={styles?.image?.width} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"image","width")})}/>
+                <UnitControl className='mt15' label="Height" value={styles?.image?.height} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"image","height")})}/>
+                  <SelectControl  className="mt15"
+                    label={__("Image Fit", "team-section")}
+                    value={styles?.image?.imageFit}
+                    options={[
+                      { value: "cover", label: "Cover" },
+                      { value: "contain", label: "Contain" },
+                      { value: "fill", label: "Fill" },
+                      { value: "scale-down", label: "Scale Down" },
+                      { value: "none", label: "None" }
+                    ]}
+                    onChange={(v)=> setAttributes({ styles: updateData(styles,v,"image","imageFit")})}/>
+
+                    <RangeControl min={1} step={0.1} max={2} className='mt15' label="Hover Scale" value={styles?.image?.hoverScal} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"image","hoverScal")})}/>
+                
+
+            </PanelBody>
+
+            <PanelBody className="bPlPanelBody"
+              title={__("Icons", "team-section")}
+              initialOpen={false}>
+
+                <ColorControl label="Overlay Color" value={styles?.icon?.overlayColor} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"icon","overlayColor")})} />
+  
+                <ColorControl label="Color" value={styles?.icon?.color} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"icon","color")})} />
+                  <ColorControl label="Hover Color" value={styles?.icon?.hoverColor} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"icon","hoverColor")})} />
+                  <ColorControl label="Hover Background" value={styles?.icon?.hoverBackgroundColor} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"icon","hoverBackgroundColor")})} />
+                  <UnitControl className='mt15' label="Width" value={styles?.icon?.width} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"icon","width")})}/>
+                  <UnitControl className='mt15' label="Height" value={styles?.icon?.height} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"icon","height")})}/>
+                    <RangeControl className='mt15' label="Icon Gap" value={styles?.icon?.gap} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"icon","gap")})} />
+
+                      <RangeControl className='mt15' label="Icon Size" value={styles?.icon?.size} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"icon","size")})} />
+
+                        <BorderControl label="Border" className='mt15' value={styles?.icon?.border} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"icon","border")})} />
+
+            </PanelBody>
             
-            <BoxControl  className='mt15' label="Radius"  values={styles?.radius} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"radius")})}/>
+      </>
+    );
+  };
 
-          
-
-          </PanelBody>
-
-          <PanelBody className="bPlPanelBody"
-            title={__("Team Profile", "team-section")}
-            initialOpen={false}>
-
-              <Background label={"Background"} value={styles?.teamProfile?.bg} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"teamProfile","bg")})} />
-
-                <BoxControl label="Padding" className="mt15" values={styles?.teamProfile?.padding} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"teamProfile","padding")})}/>
-
-                  <ColorControl label="Text Color" className="mt15" value={styles?.teamProfile?.color} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"teamProfile","color")})}/>
-
-                  <SelectControl label="Text Align" className="mt15" value={styles?.teamProfile?.textAlign} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"teamProfile","textAlign")})}
-                   options={[
-                    { value: "center", label: "Center" },
-                    { value: "left", label: "Left" },
-                    { value: "right", label: "Right" }
-                  ]}/>
-
-                     <BoxControl label="Name Margin" className="mt15" values={styles?.teamProfile?.name?.margin} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"teamProfile","name","margin")})}/>
-
-                         <Typography label="Name Typography" className="mt15" value={styles?.teamProfile?.name?.typo} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"teamProfile","name","typo")})}/>
-
-                          <BoxControl label="Designation Margin" className="mt15" values={styles?.teamProfile?.designation?.margin} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"teamProfile","designation","margin")})}/>
-
-                         <Typography label="Designation Typography" className="mt15" value={styles?.teamProfile?.designation?.typo} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"teamProfile","designation","typo")})}/>
-
-              
-
-          </PanelBody>
-
-          <PanelBody  className="bPlPanelBody"
-            title={__("Image", "team-section")}
-            initialOpen={false}>
-              
-              <UnitControl className='mt15' label="Width" value={styles?.image?.width} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"image","width")})}/>
-              <UnitControl className='mt15' label="Height" value={styles?.image?.height} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"image","height")})}/>
-                <SelectControl  className="mt15"
-                  label={__("Image Fit", "team-section")}
-                  value={styles?.image?.imageFit}
-                  options={[
-                    { value: "cover", label: "Cover" },
-                    { value: "contain", label: "Contain" },
-                    { value: "fill", label: "Fill" },
-                    { value: "scale-down", label: "Scale Down" },
-                    { value: "none", label: "None" }
-                  ]}
-                  onChange={(v)=> setAttributes({ styles: updateData(styles,v,"image","imageFit")})}/>
-
-                  <RangeControl min={1} step={0.1} max={2} className='mt15' label="Hover Scale" value={styles?.image?.hoverScal} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"image","hoverScal")})}/>
-              
-
-          </PanelBody>
-
-          <PanelBody className="bPlPanelBody"
-            title={__("Icons", "team-section")}
-            initialOpen={false}>
-
-              <ColorControl label="Overlay Color" value={styles?.icon?.overlayColor} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"icon","overlayColor")})} />
- 
-              <ColorControl label="Color" value={styles?.icon?.color} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"icon","color")})} />
-                 <ColorControl label="Hover Color" value={styles?.icon?.hoverColor} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"icon","hoverColor")})} />
-                 <ColorControl label="Hover Background" value={styles?.icon?.hoverBackgroundColor} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"icon","hoverBackgroundColor")})} />
-                 <UnitControl className='mt15' label="Width" value={styles?.icon?.width} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"icon","width")})}/>
-                 <UnitControl className='mt15' label="Height" value={styles?.icon?.height} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"icon","height")})}/>
-                  <RangeControl className='mt15' label="Icon Gap" value={styles?.icon?.gap} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"icon","gap")})} />
-
-                     <RangeControl className='mt15' label="Icon Size" value={styles?.icon?.size} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"icon","size")})} />
-
-                      <BorderControl label="Border" className='mt15' value={styles?.icon?.border} onChange={(v)=> setAttributes({ styles: updateData(styles,v,"icon","border")})} />
-
-          </PanelBody>
-          
-    </>
-  );
-};
-
-export default Style;
+  export default Style;
