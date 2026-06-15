762062595410e48eb04fe80d9624c5b0090a0769
shihab-shamim <shihabshamim767@gmail.com.com>
2026-06-15 17:00:22 +0600

    oneSetting js code align

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '1fd8ac6d5b38ff24d740');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '17d84a23177179e592a4');
